c9a70b5464e2db5fa2e8f8ee50c5d3a4dee2fdd6
pooh <cktjdgus45@naver.com>
2022-02-06 17:55:48 +0900

    add scrollIntoView to header nav button

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// gage percentage *
 const gages = document.querySelectorAll('.gage');
 
 gages.forEach(gage => {
@@ -5,3 +6,24 @@ gages.forEach(gage => {
     const percentage = parseInt(infoElement.childNodes[2].nextSibling.textContent);
     gage.style.width = `${percentage}%`;
 })
+
+//scrollTo getBoundingClientRect
+
+//document.getElementById("mine").scrollIntoView(true);
+
+
+const category = document.querySelector('.category');
+
+const goToSection = (id) => {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+const handleCategoryClicked = (event) => {
+    const clicked = event.target;
+    if (clicked.localName === 'ul') {
+        return;
+    }
+    goToSection(clicked.dataset.id);
+}
+
+category.addEventListener('click', handleCategoryClicked);
